43abd8b850994d404ceb57654691d0226d4eb257
Prajwol Chhetri <prajwalc391@gmail.com>
2022-01-03 02:33:01 +0545

    Add weekly chart

--- SmartTicketingSystem/UserControls/DashboardUC.Designer.cs
+++ SmartTicketingSystem/UserControls/DashboardUC.Designer.cs
@@ -30,22 +30,31 @@ namespace SmartTicketingSystem.UserControls
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardUC));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotalCustomers = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lblTotalEarning = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.chartCustomers = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartEarning = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCustomers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartEarning)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -54,11 +63,11 @@ namespace SmartTicketingSystem.UserControls
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.label1.Location = new System.Drawing.Point(294, 30);
+            this.label1.Location = new System.Drawing.Point(321, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(417, 34);
+            this.label1.Size = new System.Drawing.Size(331, 34);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Sales and Purchase Overview";
+            this.label1.Text = "Weekly Sales Overview";
             // 
             // label2
             // 
@@ -76,9 +85,9 @@ namespace SmartTicketingSystem.UserControls
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.BackColor = System.Drawing.Color.SeaGreen;
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblTotalCustomers);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(135, 129);
+            this.panel1.Location = new System.Drawing.Point(111, 75);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(286, 109);
             this.panel1.TabIndex = 2;
@@ -93,16 +102,16 @@ namespace SmartTicketingSystem.UserControls
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // label3
+            // lblTotalCustomers
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(45, 53);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 28);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "1000";
+            this.lblTotalCustomers.AutoSize = true;
+            this.lblTotalCustomers.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalCustomers.ForeColor = System.Drawing.Color.White;
+            this.lblTotalCustomers.Location = new System.Drawing.Point(45, 53);
+            this.lblTotalCustomers.Name = "lblTotalCustomers";
+            this.lblTotalCustomers.Size = new System.Drawing.Size(64, 28);
+            this.lblTotalCustomers.TabIndex = 2;
+            this.lblTotalCustomers.Text = "1000";
             // 
             // panel3
             // 
@@ -110,12 +119,20 @@ namespace SmartTicketingSystem.UserControls
             this.panel3.BackColor = System.Drawing.Color.DodgerBlue;
             this.panel3.Controls.Add(this.checkedListBox1);
             this.panel3.Controls.Add(this.pictureBox3);
-            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.lblTotalEarning);
             this.panel3.Controls.Add(this.label7);
-            this.panel3.Location = new System.Drawing.Point(563, 129);
+            this.panel3.Location = new System.Drawing.Point(539, 75);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(286, 109);
             this.panel3.TabIndex = 5;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(134, 108);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 85);
+            this.checkedListBox1.TabIndex = 4;
             // 
             // pictureBox3
             // 
@@ -127,16 +144,16 @@ namespace SmartTicketingSystem.UserControls
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
-            // label6
+            // lblTotalEarning
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(64, 53);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 28);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "1000";
+            this.lblTotalEarning.AutoSize = true;
+            this.lblTotalEarning.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalEarning.ForeColor = System.Drawing.Color.White;
+            this.lblTotalEarning.Location = new System.Drawing.Point(64, 53);
+            this.lblTotalEarning.Name = "lblTotalEarning";
+            this.lblTotalEarning.Size = new System.Drawing.Size(64, 28);
+            this.lblTotalEarning.TabIndex = 2;
+            this.lblTotalEarning.Text = "1000";
             // 
             // label7
             // 
@@ -149,30 +166,45 @@ namespace SmartTicketingSystem.UserControls
             this.label7.TabIndex = 1;
             this.label7.Text = "Total Earning:";
             // 
-            // button1
+            // chartCustomers
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(899, 20);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(64, 57);
-            this.button1.TabIndex = 6;
-            this.button1.UseVisualStyleBackColor = true;
+            this.chartCustomers.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea1.Name = "ChartArea1";
+            this.chartCustomers.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartCustomers.Legends.Add(legend1);
+            this.chartCustomers.Location = new System.Drawing.Point(36, 247);
+            this.chartCustomers.Name = "chartCustomers";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartCustomers.Series.Add(series1);
+            this.chartCustomers.Size = new System.Drawing.Size(438, 300);
+            this.chartCustomers.TabIndex = 7;
+            this.chartCustomers.Text = "chart1";
             // 
-            // checkedListBox1
+            // chartEarning
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(134, 108);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 85);
-            this.checkedListBox1.TabIndex = 4;
+            this.chartEarning.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea2.Name = "ChartArea1";
+            this.chartEarning.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartEarning.Legends.Add(legend2);
+            this.chartEarning.Location = new System.Drawing.Point(511, 247);
+            this.chartEarning.Name = "chartEarning";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartEarning.Series.Add(series2);
+            this.chartEarning.Size = new System.Drawing.Size(438, 300);
+            this.chartEarning.TabIndex = 8;
+            this.chartEarning.Text = "chart1";
             // 
             // DashboardUC
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chartEarning);
+            this.Controls.Add(this.chartCustomers);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
@@ -185,6 +217,8 @@ namespace SmartTicketingSystem.UserControls
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCustomers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartEarning)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,14 +229,15 @@ namespace SmartTicketingSystem.UserControls
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotalCustomers;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTotalEarning;
         private System.Windows.Forms.Label label7;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartCustomers;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartEarning;
     }
 }
--- SmartTicketingSystem/UserControls/DashboardUC.cs
+++ SmartTicketingSystem/UserControls/DashboardUC.cs
@@ -7,14 +7,141 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+using SmartTicketingSystem.Classes;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace SmartTicketingSystem.UserControls
 {
     public partial class DashboardUC : UserControl
     {
+        XmlSerializer xmlSerializer;
+        List<TicketSale> ticketSales;
+
         public DashboardUC()
         {
             InitializeComponent();
+            ticketSales = new List<TicketSale>();
+            xmlSerializer = new XmlSerializer(typeof(List<TicketSale>));
+
+            // loading data from ticket sales xml file
+            string salesXMLPath = "D:/work/year 3/Coursework/Application Dev/SmartTicketingSystem/Sales.xml";
+            if (File.Exists(salesXMLPath))
+            {
+                FileStream existingTicketSalesFS = new FileStream(salesXMLPath, FileMode.Open, FileAccess.Read);
+                ticketSales = (List<TicketSale>)xmlSerializer.Deserialize(existingTicketSalesFS);
+                existingTicketSalesFS.Close();
+            }
+
+            // loading data into chart
+            LoadReport(ticketSales);
+        }
+
+        public void LoadReport(List<TicketSale> x)
+        {
+            // function to show chart by loading data
+
+            // creating total customers and total earning as 0 in beginning
+            int totalCustomer = 0;
+            decimal totalEarning = 0;
+
+            // creating a new datatable object to link to dataGrid
+            DataTable dt = new DataTable();
+
+            // getting start and end date of week based on current date
+            var selectedDate = DateTime.Now;
+            var weekStart = selectedDate.AddDays(-(int)selectedDate.DayOfWeek);
+            var weekEnd = weekStart.AddDays(7).AddSeconds(-1);
+
+            // setting column headers
+            dt.Columns.Add("Day");
+            dt.Columns.Add("Total No Of Visitors");
+            dt.Columns.Add("Total Earning");
+
+            foreach (DateTime day in EachDay(weekStart, weekEnd))
+            {
+                // fetching total number of customer for the day
+                int customers = totalPeople(day);
+                // fetching total earning for the day
+                decimal earnings = totalPrice(day);
+
+                // adding row to datatable
+                dt.Rows.Add(day.DayOfWeek.ToString(), customers, earnings);
+
+                // adding no of people to total customer
+                totalCustomer += customers;
+
+                // adding total of sales to total earning
+                totalEarning += earnings;
+            }
+
+            // adding datatble as datasource to the datagrid in the form
+            chartCustomers.DataSource = dt;
+
+            // adding data to chart
+            chartCustomers.Series["Series1"].LegendText = "Visitors";
+            chartCustomers.Series["Series1"].ChartType = SeriesChartType.Column;
+            chartCustomers.Series["Series1"].IsValueShownAsLabel = true;
+            chartCustomers.Series["Series1"].XValueMember = "Day";
+            chartCustomers.Series["Series1"].YValueMembers = "Total No Of Visitors";
+            chartCustomers.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+            chartCustomers.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+            chartCustomers.DataSource = dt;
+
+            chartEarning.Series["Series1"].LegendText = "Price";
+            chartEarning.Series["Series1"].ChartType = SeriesChartType.Column;
+            chartEarning.Series["Series1"].IsValueShownAsLabel = true;
+            chartEarning.Series["Series1"].XValueMember = "Day";
+            chartEarning.Series["Series1"].YValueMembers = "Total Earning";
+            chartEarning.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+            chartEarning.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+            chartEarning.DataSource = dt;
+
+            // setting the value of textboxes containing total earning and total number of customer
+            lblTotalCustomers.Text = Convert.ToString(totalCustomer);
+            lblTotalEarning.Text = "Rs. " + Convert.ToString(totalEarning);
+        }
+
+        public decimal totalPrice(DateTime x)
+        {
+            // function to get total earning for particular date
+
+            decimal totalEarning = 0;
+
+            foreach (TicketSale each in ticketSales)
+            {
+                if (each.TicketDate.Date == x.Date)
+                {
+                    totalEarning += each.Total;
+                }
+            }
+
+            return totalEarning;
+        }
+
+        public int totalPeople(DateTime x)
+        {
+            // function to get total people for particular date
+
+            int totalPeople = 0;
+
+            foreach (TicketSale each in ticketSales)
+            {
+                if (each.TicketDate.Date == x.Date)
+                {
+                    totalPeople += each.NoOfPeople;
+                }
+            }
+
+            return totalPeople;
+        }
+
+        public IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
+        {
+            // custom method to iterate through days in a range of date
+            for (var day = from.Date; day.Date < thru.Date; day = day.AddDays(1))
+                yield return day;
         }
     }
 }
